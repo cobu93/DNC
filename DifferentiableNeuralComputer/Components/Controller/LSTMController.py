5e1ab8573606555402432402779c9e06920caa70
Uriel <uriel@dhcppc3>
2018-07-11 18:52:48 -0500

    LSTM controller updated

--- DifferentiableNeuralComputer/Components/Controller/LSTMController.py
+++ DifferentiableNeuralComputer/Components/Controller/LSTMController.py
@@ -18,27 +18,35 @@ class LSTMController():
 	def run_controller(self, inputs):
 	
 		self.h.clear()
+		def_layers = []
+		length = 0 
+		output_size = 0
 
 		with tf.variable_scope(self.scope):
+
+
 
 			for i in range(0, len(self.layers)):
 
 				if self.layers[i].is_input:
-					self.h[i] = Utility._concat(inputs)
-					self.h[i] = Utility._concat([self.h[i], tf.zeros([])])
+					# self.h[i] = Utility._concat(inputs)
+					# self.h[i] = Utility._concat([self.h[i], tf.zeros([])])
+					self.h[i] = tf.concat(inputs, -1) #if isinstance(inputs, list) else [inputs]
+					length = self.h[i].get_shape().as_list()[1]
 									
 				else:
-					with tf.variable_scope(self.layers[i].name):
-						
+					# tf.size(self.h[i - 1])
+					lstm_cell = tf.contrib.rnn.LSTMCell(num_units=length)
 
-						lstm_cell = tf.contrib.rnn.LSTMCell(num_units=tf.shape(self.h[i - 1])[1])
-						outputs, lstm_states = tf.nn.dynamic_rnn(lstm_cell, self.h[i - 1], dtype=tf.float32, initial_state=init_state)
+					# outputs, lstm_states = tf.nn.dynamic_rnn(lstm_cell, self.h[i - 1], dtype=tf.float32)
+					outputs, lstm_states = tf.nn.dynamic_rnn(lstm_cell, tf.reshape(self.h[i - 1], [1, 1, length]), dtype=tf.float32)
 
-						self.h[i] = Utility._concat([inputs, lstm_state.h[]])
+					self.h[i] = outputs
 
-				
-		return self.h[len(self.layers) - 1]
-		
-		
+				if self.layers[i].is_output:					
+					output_size = self.layers[i].size
+
+		return tf.reshape(self.h[len(self.layers) - 1], [1, length])[:, (length - output_size): length]
+
 
 
